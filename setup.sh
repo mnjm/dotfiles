3aecadacf9fd045110e547c39b932efe8fb558ae
MM <manjunath.bsk3@gmail.com>
2018-09-29 16:38:40 +0530

    Added Terminal theme and Updated zsh_alias.
    - xclip and rm alias are added.
    - Added Zsh Terminal theme

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Setting up the dotfiles in its specific locations
 
+# Setup Vimrc
 ln -i vimrc ~/.vimrc
+# Setup Zshrc
 ln -i zshrc ~/.zshrc
+# Setup Zsh_alias
 ln -i zsh_alias ~/.zsh_alias
+# Setup the Terminal Theme
+ln -i my_amuse ~/.oh-my-zsh/themes/amuse.zsh-theme
