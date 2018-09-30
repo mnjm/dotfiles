daafa256eff88df4996a721a77858e82ecbbcb81
Manjunath M <mnj.m0@yahoo.com>
2018-09-30 09:38:14 +0530

    Updated setup.sh.
    - Added command to source zshconfig

--- setup.sh
+++ setup.sh
@@ -2,10 +2,20 @@
 # Setting up the dotfiles in its specific locations
 
 # Setup Vimrc
+echo Setting Up Vim
 ln -i vimrc ~/.vimrc
+
 # Setup Zshrc
+echo Setting Up Zsh
 ln -i zshrc ~/.zshrc
+
 # Setup Zsh_alias
+echo Setting Alias
 ln -i zsh_alias ~/.zsh_alias
+
 # Setup the Terminal Theme
+echo Setting Up Zsh Terminal Theme
 ln -i my_amuse ~/.oh-my-zsh/themes/amuse.zsh-theme
+
+# Source Zshrc
+szrc
