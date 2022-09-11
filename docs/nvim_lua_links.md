55daaa1e5f749d91c1a921763f0013dca9297aae
Manjuanth M <manjunat.mohan@gmail.com>
2022-09-11 22:08:05 +0530

    added nvim and its lua config file

---
+++ docs/nvim_lua_links.md
@@ -0,0 +1,5 @@
+## Links useful for configuring neovim with Lua
+
+- [Lua Ref Manual](https://www.lua.org/docs.html)
+- [Neovim Lua doc](https://neovim.io/doc/user/lua.html)
+- [Neovim Lua options doc](https://neovim.io/doc/user/options.html)
--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ alert_local() {
     tput setaf $color; echo $1; tput sgr0
 }
 
-# Fresh install
-alert_local "Installing packages"
+#############################################################
+######################### Apt Packages ######################
+#############################################################
+alert_local "Installing apt packages"
 sudo apt install zsh zsh-doc tmux vim vim-gui-common curl git vlc xclip python3 python3-dev python3-pip python3-tk -y
 sudo apt install build-essential cmake android-tools-adb android-tools-fastboot -y
 sudo apt install curl wget unrar rsync -y
@@ -14,6 +16,9 @@ sudo apt install nala htop -y
 python3 -m pip install scipy numpy opencv-contrib-python matplotlib --user
 python3 -m pip install tensorflow tensorboard datetime --user
 
+#############################################################
+#################### FZF: Fuzzy File Finder #################
+#############################################################
 # Fzf
 alert_local 'Installing Fzf'
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -23,7 +28,10 @@ else
     alert_local "Fzf installation failed" 1
 fi
 
-Setup Vimrc
+#############################################################
+####################### VIM Text Editor #####################
+#############################################################
+#Setup Vimrc
 alert_local 'Setting Up Vim'
 ln -s vim/vimrc ~/.vimrc
 # Vim Plug
@@ -33,11 +41,13 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 alert_local 'Installing Vim Plugins'
 vim +PlugInstall +qall
 
+############################################################
+######################## ZSH Shell #########################
+############################################################
 # Setup Zshrc
 alert_local 'Setting Up Zsh'
 ln -s zsh/zshrc ~/.zshrc
 touch ~/.zsh_history
-
 # Setup Zsh Plugins
 mkdir -p ~/.zsh
 # Zsh Autosuggestions
@@ -45,7 +55,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 # Zsh Syntax Highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
-# Setuo the Tmux Configuration
+############################################################
+############### TMUX: Terminal Mutiplexer ##################
+############################################################
+# Setup the Tmux Configuration
 alert_local 'Setting Up Tmux Configuration'
 ln -s tmux/tmux.conf ~/.tmux.conf
 # Tmux Plugin Manager
@@ -61,6 +74,28 @@ else
     alert_local "Tpm installation failed" 1
 fi
 
+############################################################
+################### Neo-Vim Text Editor ####################
+############################################################
+# TODO Test the installation and add fail safe
+# Install and setup neovim
+alert_local 'Building and installing neovim from source'
+sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen -y
+git clone https://github.com/neovim/neovim ~/softwares/neovim
+pushd ~/softwares/neovim
+git checkout stable
+sudo make install
+# sudo cmake --build build/ --target uninstall # to Uninstall
+# Install packer.nvim (Package Manager for nvim)
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+mkdir -p ~/.config/nvim
+ln -s nvim/init.lua ~/.config/nvim/init.lua
+nvim +PackerSync +qall
+
+############################################################
+################### Change Shell  ##########################
+############################################################
 # Change shell to zsh
 alert_local "Changing default sh to zsh"
 chsh -s $(which zsh)
