92163c9ab7b63abd0acddfeb0b50e66c8568b578
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-04-02 17:15:53 +0530

    changes
    - alacritty spawn with fullscreen (F11 to window mode)
    - added new apt-packages git-filter-repo
    - modified seperator in topline
    - binded ctrl-f to fzf-file select

--- nvim/dot-config/nvim/lua/custom/plugins/topline.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/topline.lua
@@ -3,7 +3,8 @@ return {
     'mnjm/topline.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-      seperator = { pre = '', post = '' },
+      -- seperator = { pre = '', post = '' },
+      seperator = { pre = '', post = '' },
     },
   },
 }
