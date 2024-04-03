a05b01ddc45365600db3632f6f55abdbf82df433
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-04-03 11:31:44 +0530

    alacritty update
    - updated alacritty to 0.13.2
    - migrated config from yml to toml
    - updated alacritty setup file
    - added apt-instl-ad2dot

--- install/setup_alacritty.sh
+++ install/setup_alacritty.sh
@@ -11,18 +11,25 @@ _git_clone_pull https://github.com/alacritty/alacritty.git .
 
 # Build alacritty
 _alert_local "Building alacritty"
-cargo build --release
+cargo build --release --no-default-features --features=x11
+
 # Terminfo for alacritty
-sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+infocmp alacritty | sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+
 # Desktop Entry
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+_link_file target/release/alacritty ~/.local/bin/ # or anywhere else in $PATH
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
+
 # Manuals
 sudo mkdir -p /usr/local/share/man/man1
-gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+sudo mkdir -p /usr/local/share/man/man5
+scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+
 # Autocompletions
 mkdir -p ~/.zsh/zsh_functions/
 cp extra/completions/_alacritty ~/.config/zsh/zsh_functions
