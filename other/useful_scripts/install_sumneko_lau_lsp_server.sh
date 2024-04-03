375af3cfe0c5656636199b6706607ff8e4266af0
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-04-03 12:29:27 +0530

    refactored and dot- tag

--- other/useful_scripts/install_sumneko_lau_lsp_server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-. $DOTFILES/setup_utils.sh
-
-mkdir -p ~/softwares/lua-language-server
-pushd ~/softwares/lua-language-server
-curl -L https://github.com/sumneko/lua-language-server/releases/download/3.5.6/lua-language-server-3.5.6-linux-x64.tar.gz | tar xz
-_link_file ./bin/lua-language-server ~/.local/bin/lua-language-server
-popd
