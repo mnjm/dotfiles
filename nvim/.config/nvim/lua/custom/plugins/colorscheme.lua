e1895ace09cabbbac63a7317eb251b3cb21b5d32
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-04-09 07:15:13 +0530

    kanagawa nvim

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -48,8 +48,18 @@ local kanagawa = {
     priority = 1000,
     lazy = false,
     config = function()
+      require('kanagawa').setup({
+        compile = true,
+        dimInactive = true,
+        theme = "dragon",
+        terminalColors = false,
+        colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+      })
       vim.cmd.colorscheme 'kanagawa-dragon'
-      vim.cmd("hi Normal guifg=#c0c8d8 guibg=#000000")
+      vim.cmd("hi IncSearch guibg=#15F5BA guifg=#000000")
+      vim.cmd("hi Visual guibg=#15F5BA guifg=#000000")
+      vim.cmd("hi CurSearch guibg=#FDBF60 guifg=#000000")
+      vim.cmd("hi CursorLine gui=bold")
     end
   }
 }
@@ -107,12 +117,11 @@ local onedark = {
 }
 -- NOTE: Enable colorscheme here
 -- return papercolor
--- return kanagawa
+return kanagawa
 -- return tokyonight
 -- return catppuccin
 -- return nordic
 -- return onedark
-return monokai_pro
-
+-- return monokai_pro
 
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/custom/plugins/lf.lua
+++ nvim/.config/nvim/lua/custom/plugins/lf.lua
@@ -5,30 +5,19 @@ return {
       'akinsho/toggleterm.nvim'
     },
     config = function()
-      local size = 0.8
+      local size = 0.9
       local fn = vim.fn
       local o = vim.o
       require("lf").setup({
         default_action = "drop", -- default action when `Lf` opens a file
-        default_actions = { -- default action keybindings
-          ["<C-t>"] = "tabedit",
-          ["<C-x>"] = "split",
-          ["<C-v>"] = "vsplit",
-          ["<C-o>"] = "tab drop",
-        },
-
-        winblend = 10, -- psuedotransparency level
         dir = "", -- directory where `lf` starts ('gwd' is git-working-directory, ""/nil is CWD)
         direction = "float", -- window type: float horizontal vertical
-        border = "rounded", -- border kind: single double shadow curved
+        border = "shadow", -- border kind: single double shadow curved
         height = fn.float2nr(fn.round(size * o.lines)), -- height of the *floating* window
         width = fn.float2nr(fn.round(size * o.columns)), -- width of the *floating* window
         escape_quit = true, -- map escape to the quit command (so it doesn't go into a meta normal mode)
         focus_on_open = true, -- focus the current file when opening Lf (experimental)
-        mappings = true, -- whether terminal buffer mapping is enabled
-        tmux = false, -- tmux statusline can be disabled on opening of Lf
-        default_file_manager = false, -- make lf default file manager
-        disable_netrw_warning = false, -- don't display a message when opening a directory with `default_file_manager` as true
+        mappings = false, -- whether terminal buffer mapping is enabled
         highlights = { -- highlights passed to toggleterm
           Normal = {link = "Normal"},
           NormalFloat = {link = 'Normal'},
