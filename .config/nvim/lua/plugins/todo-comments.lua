939ba132e5deaba7622ce6ebeb1ba99c2177a0cc
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-07-01 19:09:06 +0530

    nvim: readded todo-comment hls

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -11,33 +11,16 @@
 --                    \______/
 -- Author - mnjm - github.com/mnjm
 -- Repo - github.com/mnjm/dotfiles
--- derived from kickstart.nvim
 
--- Install Lazy plugin manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({
-  'tpope/vim-surround', -- operates on surrounding objs like ysiw) adds () around inner world
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_fonts },
-  require 'plugins/todo-comments',
-  require 'plugins/gitsigns',
-  require 'plugins/which-key',
-  require 'plugins/telescope',
-  require 'plugins/lspconfig',
-  require 'plugins/cmp',
-  require 'plugins/treesitter',
-  require 'plugins/colorscheme',
-  require 'plugins/bottomline',
-  require 'plugins/topline',
-  require 'plugins/winline',
-  require 'plugins/oil',
-  require 'plugins/tmux-nav',
-  require 'plugins/lf',
-})
+-- Highlight todo, notes, etc in comments
+return {
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+    },
+  },
+}
 -- vim: ts=2 sts=2 sw=2 et
