78cda9e24805b4b8105db65d1c30f8133154b93f
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-07-03 14:35:25 +0530

    nvim: added vim-doge

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -177,6 +177,7 @@ return {
             }
           }
         },
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
