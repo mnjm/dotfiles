ef9fffc660c6716184f0ab51724afb6cae6bbc64
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-08-15 08:30:51 +0530

    nvim: which-key update

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -35,18 +35,16 @@ return {
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument/[D]iff', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      require('which-key').add {
+        {'<leader>c', group = '[C]ode' },
+        {'<leader>d', group = '[D]ocument/[D]iff' },
+        {'<leader>r', group = '[R]ename' },
+        {'<leader>s', group = '[S]earch' },
+        {'<leader>w', group = '[W]orkspace' },
+        {'<leader>f', group = '[F]ind' },
+        {'<leader>t', group = '[T]oggle' },
+        {'<leader>', group = 'VISUAL <leader>', mode = 'v' },
       }
-      require('which-key').register({
-        ['<leader>'] = { name = 'VISUAL <leader>' },
-      }, { mode = 'v' })
     end,
   },
 }
